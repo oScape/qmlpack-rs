29737a4e003f6444b960a43a4c01221de09dbb9b
b-chassignol <bchassignol.externe@m6.fr>
2019-11-10 18:17:28 +0100

    json parse

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
+use serde_json::error::Error as SerdeError;
+use serde_json::{from_str, Error, Value};
 use std::process::Command;
-use std::io::{Write};
-use std::fs::File;
+use std::string::FromUtf8Error;
 
-fn main() {
+#[derive(Debug)]
+enum MyError {
+    Utf8Error(FromUtf8Error),
+    JsonError(SerdeError),
+}
+
+impl From<FromUtf8Error> for MyError {
+    fn from(err: FromUtf8Error) -> Self {
+        MyError::Utf8Error(err)
+    }
+}
+
+impl From<SerdeError> for MyError {
+    fn from(err: SerdeError) -> Self {
+        MyError::JsonError(err)
+    }
+}
+
+fn main() -> Result<(), MyError> {
     let output = Command::new("./bin/qml-parser")
         .arg("./sandbox/1/new.qml")
         .output()
         .expect("bin command failed to start");
 
-    File::create("./sandbox/data.json")
-        .expect("Impossible to create json file")
-        .write(&output.stdout)
-        .expect("Impossible to write in the file");
+    let json_result = untyped_json(String::from_utf8(output.stdout)?)?;
+
+    println!("Json: \n {:#}", json_result);
+
+    Ok(())
+}
+
+fn untyped_json(output: String) -> Result<Value, Error> {
+    let v = from_str(&output)?;
+    Ok(v)
 }
